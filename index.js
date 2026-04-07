86a28ca353f47cafb60fc4966a67dcd2ab4ba77f
donasher-47f <ckipchirchirasher@gmail.com>
2026-04-07 12:55:26 +0300

    modified 1 file

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
+// combineUsers
 
+const username1 = "theblackkingshow";
 
+for (const _ of Array(3).keys()) {
+    console.log(username1);
+}
+const username2 = "mafiaboss";
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+for (const _ of Array(5).keys()) {
+    console.log(username2);
+}
+
+function combineUsernames(username1, username2) {
+    return `${username1} & ${username2}`;
+}
+
+// combineObjects
+
+const user1 = {
+    name: "theblackkingshow",
+    age: 20,
+    city: "Atlanta"
 };
+
+const user2 = {
+    name: "mafiaboss",
+    age: 19,
+    city: "Los Angeles",
+
+    datejs: "03/04/2026"
+};
+
+// require the datejs module
+const datejs = require("datejs");
+
+// return object
+function combineUserObjects(user1, user2) {
+    return {
+        name: `${user1.name} & ${user2.name}`,
+    };
+}
